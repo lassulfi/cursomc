59c1a9da0dee1867fb654f0b85b5d5523e5d286e
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-03-19 20:36:14 -0300

    Validação customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/luisassulfi/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/luisassulfi/cursomc/dto/ClienteNewDTO.java
@@ -2,20 +2,42 @@ package com.luisassulfi.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.luisassulfi.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message="Campo nome deve ser preenchido")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Campo email deve ser preenchido")
+	@Email(message="E-mail inválido")
 	private String email;
+	
+	@NotEmpty(message="Campo cpfOuCnpj deve ser preenchido")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
 	
+	@NotEmpty(message="Campo logradouro deve ser preenchido")
 	private String logradouro;
+	
+	@NotEmpty(message="Campo numero deve ser preenchido")
 	private String numero;
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Campo cep deve ser preenchido")
 	private String cep;
 	
+	@NotEmpty(message="Campo telefone 1 deve ser preenchido")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
