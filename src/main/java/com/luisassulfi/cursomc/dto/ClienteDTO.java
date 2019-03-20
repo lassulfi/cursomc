a619fe4d8efe7222aee802c369175f4eee65238e
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-03-19 21:03:16 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/luisassulfi/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/luisassulfi/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.luisassulfi.cursomc.domain.Cliente;
+import com.luisassulfi.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
