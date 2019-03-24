c40be22d896b818cff4f5e5d8bb6d33e3b7c74cd
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-03-24 08:46:45 -0300

    MockEmailService com Logger: Padroes Strategy e Template

--- src/main/java/com/luisassulfi/cursomc/config/TestConfig.java
+++ src/main/java/com/luisassulfi/cursomc/config/TestConfig.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.luisassulfi.cursomc.services.DBService;
+import com.luisassulfi.cursomc.services.EmailService;
+import com.luisassulfi.cursomc.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -21,5 +23,10 @@ public class TestConfig {
 		service.instantiateTestDatabase();
 		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
--- src/main/java/com/luisassulfi/cursomc/services/PedidoService.java
+++ src/main/java/com/luisassulfi/cursomc/services/PedidoService.java
@@ -36,6 +36,9 @@ public class PedidoService {
 	
 	@Autowired
 	private ClienteService clienteService;
+	
+	@Autowired
+	private EmailService emailService;
 
 	public Pedido findById(Integer id) {
 
@@ -65,7 +68,8 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);		
+		emailService.sendOrderConfirmationEmail(obj);
+			
 		return obj;
 	}
 }
