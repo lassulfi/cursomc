e7fa496219f4b186e960e1033f8d2a4febc3571a
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-03-26 12:52:01 -0300

    Adicionando senha ao cliente

--- src/main/java/com/luisassulfi/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/luisassulfi/cursomc/dto/ClienteNewDTO.java
@@ -26,6 +26,9 @@ public class ClienteNewDTO implements Serializable {
 	
 	private Integer tipo;
 	
+	@NotEmpty(message="Campo senha não pode ser vazio")
+	private String senha;
+	
 	@NotEmpty(message="Campo logradouro deve ser preenchido")
 	private String logradouro;
 	
@@ -77,6 +80,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public String getLogradouro() {
--- src/main/java/com/luisassulfi/cursomc/services/DBService.java
+++ src/main/java/com/luisassulfi/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.luisassulfi.cursomc.domain.Categoria;
@@ -59,6 +60,9 @@ public class DBService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 
 	public void instantiateTestDatabase() throws ParseException {
 		Categoria cat1 = new Categoria(null, "Informática");
@@ -117,7 +121,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Luis Daniel Assulfi", "lassulfi@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Luis Daniel Assulfi", "lassulfi@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, encoder.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", c1, cli1);
