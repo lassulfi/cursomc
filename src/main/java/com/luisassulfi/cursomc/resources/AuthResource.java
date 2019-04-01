11899fe173e0860cd4f7dedb221d686ae7562eb2
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-04-01 13:16:43 -0300

    Refresh token

---
+++ src/main/java/com/luisassulfi/cursomc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.luisassulfi.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luisassulfi.cursomc.security.JWTUtil;
+import com.luisassulfi.cursomc.security.UserSS;
+import com.luisassulfi.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		
+		return ResponseEntity.noContent().build();
+	}
+}
