895cd2e06f6c00ba3aa8d97b677ba541f816cdbd
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-04-04 20:37:56 -0300

    Ajustando o tamanho da imagem

--- src/main/java/com/luisassulfi/cursomc/services/ClienteService.java
+++ src/main/java/com/luisassulfi/cursomc/services/ClienteService.java
@@ -50,6 +50,9 @@ public class ClienteService {
 	
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Cliente findById(Integer id) {
 
@@ -132,6 +135,8 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		
 		String filename = prefix + user.getId() + ".jpg";
 		
