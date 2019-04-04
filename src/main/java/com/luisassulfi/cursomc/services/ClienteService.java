ac937d6cf199faef3a237a8db120fd377a4f5ce3
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-04-03 22:08:31 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/luisassulfi/cursomc/services/ClienteService.java
+++ src/main/java/com/luisassulfi/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.luisassulfi.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 
 	public Cliente findById(Integer id) {
 
@@ -123,15 +131,10 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgFromFile(multipartFile);
 		
-		Optional<Cliente> obj = clienteRepository.findById(user.getId());
-		if(obj.isPresent()) {
-			Cliente cli = obj.get();
-			cli.setImageUrl(uri.toString());
-			clienteRepository.save(cli);
-		}
+		String filename = prefix + user.getId() + ".jpg";
 		
-		return uri;
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), filename, "image");
 	}
 }
